35e63bc6d24b45e1c967dbb7ec1812d5bfbe6451
1115738 <128939248+1115738@users.noreply.github.com>
2023-04-06 11:01:59 -0400

    Bloques nuevos

---
+++ LIBlock.cs
@@ -0,0 +1,17 @@
+﻿namespace WpfApp1
+{
+    public class LIBlock : Block
+    {
+        private readonly Position[][] tiles = new Position[][]
+        {
+            new Position[] { new(2, 0), new(2, 1), new(2, 2), new(2, 3), new(2, 4)},
+            new Position[] { new(0, 2), new(1, 2), new(2, 2), new(3, 2), new(4, 2)}
+        };
+
+        public override int Id => 8;
+
+        protected override Position StartOffSet => new Position(0, 3);
+
+        protected override Position[][] Tiles => tiles;
+    }
+}
